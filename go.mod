3a9c30a4ee3b0138e3d2e51740c02e24dc6605ec
edualt <eduardosebastianhdz503@gmail.com>
2023-10-28 19:13:13 -0600

    main logic complete

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module simulador
 
 go 1.21.0
+
+require fyne.io/fyne/v2 v2.4.1 // indirect
--- models/car.go
+++ models/car.go
@@ -1,4 +1,37 @@
 package models
 
 type Car struct {
+	Id           int
+	IsWaiting    bool
+	IsParked     bool
+	IsEntering   bool
+	IsGettingOut bool
+}
+
+func NewCar(id int, isWaiting, isParked, isEntering, isGettingOut bool) *Car {
+	return &Car{
+		Id:           id,
+		IsWaiting:    isWaiting,
+		IsParked:     isParked,
+		IsEntering:   isEntering,
+		IsGettingOut: isGettingOut,
+	}
+}
+
+func (c *Car) GetID() int {
+	return c.Id
+}
+
+func (c *Car) GetStatus() string {
+	status := "Unknown"
+	if c.IsWaiting {
+		status = "Waiting"
+	} else if c.IsParked {
+		status = "Parked"
+	} else if c.IsEntering {
+		status = "Entering"
+	} else if c.IsGettingOut {
+		status = "Getting Out"
+	}
+	return status
 }
